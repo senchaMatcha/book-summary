ac894df6b2f61458d1696056d08f5dd59d782525
SenchaMacha <protoweb88@outlook.com>
2024-12-07 06:58:54 -0800

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+const Page = () => {
+	return (
+		<div
+			initial={{}}
+			animate={{}}
+			exit={{}}
+			transition={{}}
+			className='bg-slate-50 w-screen  min-h-screen h-auto overflow-x-hidden'>
+			<header className='text-gray-500 font-semibold text-5xl text-center w-full max-w-4xl px-4 py-8'>
+				The Library
+			</header>
+		</div>
+	);
+};
+export default Page;
